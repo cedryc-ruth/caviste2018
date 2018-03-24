cbdfca504fe7c7583bf56fb662960df4df83c0e1
Cédryc Ruth <ceruth@domepfc.net>
2018-03-24 13:28:49 +0100

    fixes #14, Correction indentation

--- client/public_html/js/app.js
+++ client/public_html/js/app.js
@@ -239,20 +239,21 @@ $(document).ready(function() {
     $('#btDeleteWine').on('click', function() {
         //Retirer la notification d'erreur
         removeError();
-            let vin = getFormData();
-            if(confirm('Confirmer la suppression ?')){        
+        let vin = getFormData();
+        
+        if(confirm('Confirmer la suppression ?')){        
             $.ajax({
                 'method':'DELETE',
                 'url':API_URL+'/wines/'+vin.id,
             }).done(function(data) {
-            if(data) {
-            //Actualiser la liste de tous les vins
-                showWines();
-                reportError('Le vin a bien été supprimé.','success');
-            }else {
-            
-                reportError('Désolé, Impossible de supprimer ce vin!','error');
-            }
+                if(data) {
+                //Actualiser la liste de tous les vins
+                    showWines();
+                    reportError('Le vin a bien été supprimé.','success');
+                }else {
+
+                    reportError('Désolé, Impossible de supprimer ce vin!','error');
+                }
             }).fail(function() {
                         reportError('Désolé, Impossible de supprimer ce vin!','error'); 
             });
